d958c76a3875bdd67658e61c32f7f28b94fb11b7
addid10 <rezkynewaditya@gmail.com>
2019-11-04 19:27:07 +0800

    add sweetalert2.css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,30 +9,7 @@
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
-
-    <style>
-            /* Fonts */
-            @font-face {
-                font-family: 'ubuntu-light';
-                src: url("{{ asset('assets/fonts/Ubuntu/Ubuntu-Light.eot') }}") !important;
-            }
-    
-            @font-face {
-                font-family: 'ubuntu-regular';
-                src: url("{{ asset('assets/fonts/Ubuntu/Ubuntu-Regular.eot') }}") !important;
-            }
-    
-            @font-face {
-                font-family: 'ubuntu-medium';
-                src: url("{{ asset('assets/fonts/Ubuntu/Ubuntu-Medium.eot') }}") !important;
-            }
-    
-            @font-face {
-                font-family: 'ubuntu-bold';
-                src: url("{{ asset('assets/fonts/Ubuntu/Ubuntu-Bold.eot') }}") !important;
-            }
-    
-        </style>
+    <link rel="stylesheet" href="{{ asset('assets/vendors/sweetalert2/sweetalert2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     {{-- Font  --}}
