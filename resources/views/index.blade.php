e4de9b253b1a52fb1d89ca650bacb28c69924563
addid10 <rezkynewaditya@gmail.com>
2019-11-04 19:29:45 +0800

    notify when user login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,15 @@
 
 @section('title', 'Dashboard')
 
+
 @section('content')
+<div class="row" id="proBanner">
+    <div class="col-12">
+        <span class="d-flex align-items-center purchase-popup">
+            <p>You have logged in as an </p>
+                <a
+                    class="btn purchase-button ml-2">admin</a>
+        </span>
+    </div>
+</div>
 @endsection
