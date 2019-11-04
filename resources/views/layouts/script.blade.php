6d3dfe268ba7c91f4c9a4aca4fc3b915ac49bd8e
addid10 <rezkynewaditya@gmail.com>
2019-11-04 19:26:38 +0800

    add sweetalert2.js

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,5 +1,6 @@
 <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
 <script src="{{ asset('assets/vendors/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/sweetalert2/sweetalert2.all.min.js') }}"></script>
 <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
 <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
 <script src="{{ asset('assets/js/misc.js') }}"></script>
