c4fb276d4cc26b1b901ee4e82367e8f04674ac20
addid10 <rezkynewaditya@gmail.com>
2019-11-04 19:31:21 +0800

    add delete js in character and visual novel

---
+++ public/js/character/character.js
@@ -0,0 +1,30 @@
+$('#character-table tbody').on('click', '.delete', function () {
+    let id = $(this).attr("id");
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            // $.ajax({
+            //     url: "lecturers/" + id,
+            //     type: 'DELETE',
+            //     success: function () {
+            Swal.fire(
+                    'Deleted Id ' + id + '!',
+                    'Visual novel has been turned off',
+                    'success'
+                )
+                .then(function () {
+                    dataTable.ajax.reload();
+                });
+            //     }
+            // });
+        }
+    })
+});
