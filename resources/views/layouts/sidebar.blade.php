795dddf3efb0035ce564a5312d416793e43523df
addid10 <rezkynewaditya@gmail.com>
2019-11-06 03:36:03 +0800

    add auth data

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,13 +3,13 @@
         <li class="nav-item nav-profile">
             <a href="#" class="nav-link">
                 <div class="nav-profile-image">
-                    <img src="{{ asset('assets/images/faces/face1.jpg') }}" alt="profile">
+                    <img src="{{ Auth::user()->image_profile }}" alt="profile">
                     {{-- Busy, Offline, Online --}}
                     <span class="login-status offline"></span>
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                    <span class="font-weight-bold mb-2">David Grey. H</span>
-                    <span class="text-secondary text-small">Project Manager</span>
+                    <span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span>
+                    <span class="text-secondary text-small">{{ ucfirst(Auth::user()->role->name) }}</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
             </a>
@@ -27,6 +27,7 @@
             </a>
 
         </li>
+        @if(Auth::user()->role->name === "creator")
         <li class="nav-item {{ Request::is('visual_novels*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('visual_novels.index') }}">
                 <span class="menu-title">Visual Novel</span>
@@ -48,6 +49,7 @@
                 </ul>
             </div>
         </li>
+        @endif
         <li class="nav-item {{ Request::is('stories') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('stories.index') }}">
                 <span class="menu-title">Visual Novel Stories</span>
