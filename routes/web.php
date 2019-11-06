36479ae22af042fee2fb7f641aa334423252b801
addid10 <rezkynewaditya@gmail.com>
2019-11-06 13:09:23 +0800

    add some route

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,17 @@
 */
 Route::group([ 'middleware' => ['auth','role:creator']], function () {
     
-    Route::view('/visual_novels', 'visual_novels.index')->name('visual_novels.index');
+    Route::prefix('visual_novels')->group(function () {
+        Route::get('/', 'VisualNovelController@index')->name('visual_novels.index');
+        Route::get('/to-json', 'VisualNovelController@toJson');
+    });
 
     Route::view('/stories', 'stories.index')->name('stories.index');
 
+
     Route::prefix('assets')->group(function () {
-        
-
         Route::view('/characters', 'characters.index')->name('characters.index');
-
         Route::view('/backgrounds', 'backgrounds.index')->name('backgrounds.index');
-
         Route::view('/musics', 'musics.index')->name('musics.index');   
     });
 });
@@ -31,16 +31,13 @@ Route::group([ 'middleware' => ['auth','role:creator&guest star']], function () 
 
     Route::view('/', 'index')->name('dashboard.index');
 
-
-
-    
-
     Route::prefix('game')->group(function () {
         Route::get('/', 'GameController@index')->name('game.index');
-        Route::view('/menu', 'games.menu')->name('game.menu');
-        Route::view('/play', 'games.play')->name('game.play');
+        Route::get('{id}/menu', 'GameController@menu')->name('game.menu');
+        // Route::get('/play', 'games.play')->name('game.play');
         
     });
+
 });
 
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login.index');
