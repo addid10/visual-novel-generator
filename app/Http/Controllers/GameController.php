58f6a6bd54683ebbaaecaab1ad55e74d94fa5708
addid10 <rezkynewaditya@gmail.com>
2019-11-06 13:12:54 +0800

    add menu in GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\VisualNovel;
+use App\SaveData;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
+
     public function index()
     {
         $games = VisualNovel::with('genres')->get();
         
         return view('games.index', ['games' => $games]);
+    }
+
+    public function menu($id)
+    {        
+        $userId = Auth::user()->id;
+        
+        $menu = SaveData::with(['story' => function($story){
+            $story->whereVisualNovelId($id);
+        }])
+        ->find($userId);
+
+
+        return view('games.menu', ['menu' => $menu]);
     }
 }
