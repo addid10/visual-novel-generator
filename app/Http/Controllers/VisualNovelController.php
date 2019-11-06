6d106f67accf811033a2a19456f5368f00649317
addid10 <rezkynewaditya@gmail.com>
2019-11-06 13:13:22 +0800

    add json data in VisualNovelController

--- app/Http/Controllers/VisualNovelController.php
+++ app/Http/Controllers/VisualNovelController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\VisualNovel;
 use Illuminate\Http\Request;
 
 class VisualNovelController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('visual_novels.index');
+    
+    }
+
+    public function toJson()
+    {
+        $visual_novels = VisualNovel::with('user')->get();
+
+        return response()->json([
+            'data' => $visual_novels
+        ]);
+    }
+
 }
