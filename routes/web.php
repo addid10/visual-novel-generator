95d50ae11af9e03bc3277c13a6f5e3359bd9d4f2
addid10 <rezkynewaditya@gmail.com>
2019-11-06 11:17:31 +0800

    add middleware in route

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::view('/', 'index')->name('dashboard.index');
-
-Route::view('/visual_novels', 'visual_novels.index')->name('visual_novels.index');
-
-Route::view('/lists', 'stories.list')->name('stories.list');
-
-Route::prefix('assets')->group(function () {
-
-    Route::view('/characters', 'characters.index')->name('characters.index');
-
-    Route::view('/backgrounds', 'backgrounds.index')->name('backgrounds.index');
-
-    Route::view('/musics', 'musics.index')->name('musics.index');   
-});
-
-Route::prefix('stories')->group(function () {
-    Route::view('/', 'stories.index')->name('stories.index');
-    Route::view('/menu', 'stories.menu')->name('stories.menu');
-    Route::view('/play', 'stories.play')->name('stories.play');
+Route::group([ 'middleware' => ['auth','role:creator']], function () {
     
+    Route::view('/visual_novels', 'visual_novels.index')->name('visual_novels.index');
+
+    Route::view('/stories', 'stories.index')->name('stories.index');
+
+    Route::prefix('assets')->group(function () {
+        
+
+        Route::view('/characters', 'characters.index')->name('characters.index');
+
+        Route::view('/backgrounds', 'backgrounds.index')->name('backgrounds.index');
+
+        Route::view('/musics', 'musics.index')->name('musics.index');   
+    });
 });
 
-Route::view('/login', 'users.login')->name('login.index');
+Route::group([ 'middleware' => ['auth','role:creator&guest star']], function () {
+
+    Route::view('/', 'index')->name('dashboard.index');
+
+
+
+    
+
+    Route::prefix('game')->group(function () {
+        Route::get('/', 'GameController@index')->name('game.index');
+        Route::view('/menu', 'games.menu')->name('game.menu');
+        Route::view('/play', 'games.play')->name('game.play');
+        
+    });
+});
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login.index');
+Route::post('/login', 'Auth\LoginController@login')->name('login.store');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
