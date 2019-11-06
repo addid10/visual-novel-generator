dda891c9bd122d879c00a71f0040898ebde25c2f
addid10 <rezkynewaditya@gmail.com>
2019-11-06 19:59:07 +0800

    make store function in visual_novels

--- app/Http/Controllers/VisualNovelController.php
+++ app/Http/Controllers/VisualNovelController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Auth; 
 use App\VisualNovel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VisualNovelController extends Controller
 {
     public function index()
     {
-
+        
         return view('visual_novels.index');
     
     }
@@ -21,6 +23,58 @@ class VisualNovelController extends Controller
         return response()->json([
             'data' => $visual_novels
         ]);
+    }
+
+    
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'title' => 'required|string|unique:visual_novels,title',
+            ]);
+
+            if($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->errors()
+                ]);
+            }
+
+            VisualNovel::create([
+                'title' => $request->title,
+                'user_id' => Auth::user()->id,
+                'synopsis' => $request->synopsis
+
+            ])
+            ->genres()->attach($request->genres);
+            
+            return response()->json([
+                'success' => "Data added successfully!"
+            ]);
+       
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => $th
+            ]);
+        }
+
+    }
+    
+    
+    public function update(Request $request, $id)
+    {
+        $visualNovel = VisualNovel::findOrFail($id);
+
+        $visualNovel->update($request->all())
+        ->genres()->sync($request->topics);
+    }
+
+
+
+    public function destroy($id)
+    {
+        $visualNovel = VisualNovel::findOrFail($id);
+
+        $visualNovel->forceDelete();
     }
 
 }
