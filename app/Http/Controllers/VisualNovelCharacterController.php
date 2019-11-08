e0970e4a9dd20cb63cc0388115650acce1258d0a
addid10 <rezkynewaditya@gmail.com>
2019-11-09 00:12:17 +0800

    create store & destroy for character visual novel

--- app/Http/Controllers/VisualNovelCharacterController.php
+++ app/Http/Controllers/VisualNovelCharacterController.php
@@ -2,20 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\VisualNovel;
 use App\VisualNovelCharacter;
+use App\Helpers\VisualNovelHelper;
+
 use Illuminate\Http\Request;
 
 class VisualNovelCharacterController extends Controller
 {
+    public function store(Request $request)
+    {
+        try {
+
+            $validator = VisualNovelHelper::checkCharacter($request->visual_novel_id, $request->character_id);
+
+            if($validator) {
+                VisualNovelCharacter::create([
+                    'visual_novel_id' => $request->visual_novel_id,
+                    'character_id' => $request->character_id,
+                    'character_role_id' => $request->character_role_id
+                ]);
+                
+                return response()->json([
+                    'success' => "Data added successfully!"
+                ]);
+
+            } else {
+                
+                return response()->json([
+                    'error' => "The character has already been taken!"
+                ]);
+            }
+       
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => "Failed data execution!"
+            ]);
+        }
+
+    }
     public function show($id)
     {
-        $characterVisualNovel = VisualNovelCharacter::with(['visual_novel', 'character' => function($query){
+        $visualNovelCharacters = VisualNovelCharacter::with(['visual_novel', 'character' => function($query){
             $query->with('characters_images');
         }, 'character_role'])
         ->whereVisualNovelId($id)->get();
 
         return response()->json([
-            'data' => $characterVisualNovel
+            'data' => $visualNovelCharacters
         ]);
+    }
+
+    public function destroy($id)
+    {
+        $visualNovelCharacters = VisualNovelCharacter::findOrFail($id);
+
+        $visualNovelCharacters->delete();
+    }
+
+    public function iseng(){
+        
+        $visualNovelBackground = VisualNovel::with('backgrounds')
+        ->get();
+
+        dd($visualNovelBackground);
+        if ($visualNovelBackground === 0){
+            return "T";
+        } else {
+            return "F";
+        }
     }
 }
