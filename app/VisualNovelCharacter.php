d867c0458454f772efa57107bb7d01fa60174cff
addid10 <rezkynewaditya@gmail.com>
2019-11-09 00:10:42 +0800

    add fillable table in visual novel character model

--- app/VisualNovelCharacter.php
+++ app/VisualNovelCharacter.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class VisualNovelCharacter extends Model
 {
     protected $table = 'visual_novels_characters';
-
     public $timestamps = false;
+
+    protected $fillable = ['visual_novel_id', 'character_id', 'character_role_id'];
     
     public function character_role()
     {
