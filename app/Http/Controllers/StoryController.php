0eb04cd14e7ac86944f4e8bf998773b84b8932fc
addid10 <rezkynewaditya@gmail.com>
2019-11-09 00:13:35 +0800

    create function for read/browse index dataTable

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\VisualNovel;
 use Illuminate\Http\Request;
 
 class StoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $stories = VisualNovel::with('user')->get();
+
+        return $request->ajax() ? response()->json(['data' => $stories]) : view('stories.index');
+    }
 }
