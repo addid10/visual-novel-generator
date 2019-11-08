a3e7fa5f63c2ae1814e17c3d5903567f091f3b8f
addid10 <rezkynewaditya@gmail.com>
2019-11-09 00:13:14 +0800

    add dataTable on stories

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.template')
 
-@section('title', 'Visual Novels')
+@section('css')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="{{ asset('assets/vendors/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+@endsection
+
+@section('title', 'Visual Novels Stories')
 
 @section('content')
 <div class="row">
     <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Visual Novels</h4>
+                <h4 class="card-title">Visual Novels Stories</h4>
                 <div class="table-responsive">
                     <table class="table" id="stories-table">
                         <thead>
@@ -18,14 +23,6 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>GSA Visual Novel</td>
-                                <td>Rerezditya</td>
-                                <td>
-                                    <button type="button" class="btn btn-md btn-gradient-primary w-100" data-toggle="modal"
-                                        data-target="#stories-modal">Stories</button>
-                                </td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
@@ -41,5 +38,20 @@
 @endsection
 
 @section('javascript')
+{{-- Datatable  --}}
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables-bs4/js/jquery.dataTables.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+
+{{-- Custom Javascript --}}
     <script src="{{ asset('js/stories/stories.js') }}"></script>
 @endsection
+
+{{-- 
+<tr>
+    <td>GSA Visual Novel</td>
+    <td>Rerezditya</td>
+    <td>
+        <button type="button" class="btn btn-md btn-gradient-primary w-100" data-toggle="modal"
+            data-target="#stories-modal">Stories</button>
+    </td>
+</tr> --}}
