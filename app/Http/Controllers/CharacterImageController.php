5b9f8aa3fd6d7039a793b018d2055086feeaf460
addid10 <rezkynewaditya@gmail.com>
2019-11-09 11:34:26 +0800

    add index method

--- app/Http/Controllers/CharacterImageController.php
+++ app/Http/Controllers/CharacterImageController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\CharacterImage;
 use Illuminate\Http\Request;
 
 class CharacterImageController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $charactersImages = CharacterImage::get(['id', 'image']);
+
+        return $request->ajax() ? response()->json(['data' => $charactersImages]) : view('characters.index');
+    }
+        
 }
