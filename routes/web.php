f9009247eb0d2e6cf231b5a62cf13a163b0b58f4
addid10 <rezkynewaditya@gmail.com>
2019-11-09 11:28:39 +0800

    change route parameter name

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,30 @@ Route::group([ 'middleware' => ['auth','role:creator']], function () {
     // Route::get('/creators', 'UserController@index');
     Route::get('/genres', 'GenreController@index');
     
-    Route::prefix('visual_novels')->group(function () {
-        Route::get('/', 'VisualNovelController@index')->name('visual_novels.index');
-        Route::post('/', 'VisualNovelController@store')->name('visual_novels.store');
-        Route::put('/{visual_novel}', 'VisualNovelController@update')->name('visual_novels.update');
-        Route::delete('/{visual_novel}', 'VisualNovelController@destroy')->name('visual_novels.destroy');
-        Route::get('/{visual_novel}/edit', 'VisualNovelController@edit')->name('visual_novels.edit');
+    Route::prefix('visual-novels')->group(function () {
+        Route::get('/', 'VisualNovelController@index')->name('visual-novels.index');
+        Route::post('/', 'VisualNovelController@store')->name('visual-novels.store');
+        Route::put('/{visualNovel}', 'VisualNovelController@update')->name('visual-novels.update');
+        Route::delete('/{visualNovel}', 'VisualNovelController@destroy')->name('visual-novels.destroy');
+        Route::get('/{visualNovel}/edit', 'VisualNovelController@edit')->name('visual-novels.edit');
 
-        Route::get('/iseng', 'VisualNovelCharacterController@iseng')->name('visual_novel_characters.iseng');
-        Route::post('/characters', 'VisualNovelCharacterController@store')->name('visual_novel_characters.store');
-        Route::delete('/characters/{character}', 'VisualNovelCharacterController@destroy')->name('visual_novel_characters.destroy');
-        Route::get('/{visual_novel}/characters', 'VisualNovelCharacterController@show')->name('visual_novel_characters.show');
+        Route::get('/iseng', 'VisualNovelCharacterController@iseng')->name('visual-novel-characters.iseng');
+        Route::post('/characters', 'VisualNovelCharacterController@store')->name('visual-novel-characters.store');
+        Route::delete('/characters/{character}', 'VisualNovelCharacterController@destroy')->name('visual-novel-characters.destroy');
+        Route::get('/{visualNovel}/characters', 'VisualNovelCharacterController@show')->name('visual-novel-characters.show');
     });
 
     Route::prefix('stories')->group(function () {
         Route::get('/', 'StoryController@index')->name('stories.index');
-        Route::get('/{visual_novel}', 'StoryController@show')->name('stories.show');
+        Route::post('/', 'StoryController@store')->name('stories.store');
+        Route::get('/{visualNovel}', 'StoryController@show')->name('stories.show');
     });
 
     Route::prefix('assets')->group(function () {
         Route::get('/characters', 'CharacterController@index')->name('characters.index');
+        Route::get('/characters-images', 'CharacterImageController@index')->name('characters-images.index');   
+
+
         Route::get('/backgrounds', 'BackgroundController@index')->name('backgrounds.index');
         Route::get('/musics', 'MusicController@index')->name('musics.index');   
     });
