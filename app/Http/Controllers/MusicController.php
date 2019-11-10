b18c071952aac4d54e160932431554497ab5f8b2
addid10 <rezkynewaditya@gmail.com>
2019-11-10 18:40:23 +0800

    fix index() for specific visual novel

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\VisualNovel;
 use App\Music;
+
 use Illuminate\Http\Request;
 
 class MusicController extends Controller
 {
     public function index(Request $request)
     {
-        $musics = Music::get(['id', 'name', 'music']);
+        $id = $request->id;
+
+        $musics = VisualNovel::with('musics')->whereId($id)->first(['id']);
 
         return $request->ajax() ? response()->json(['data' => $musics]) : view('musics.index');
     }
