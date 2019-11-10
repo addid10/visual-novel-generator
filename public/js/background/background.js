2e16cd104497d241e7818753d804b1025c58493b
addid10 <rezkynewaditya@gmail.com>
2019-11-10 19:21:00 +0800

    delete ajax request for delete

--- public/js/background/background.js
+++ public/js/background/background.js
@@ -37,35 +37,3 @@ let dataTable = $('#background-table').DataTable({
         }
     ]
 });
-
-
-$('#background-table tbody').on('click', '.delete', function () {
-    let id = $(this).attr("id");
-
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.value) {
-            // $.ajax({
-            //     url: "lecturers/" + id,
-            //     type: 'DELETE',
-            //     success: function () {
-            Swal.fire(
-                    'Deleted Id ' + id + '!',
-                    'Visual novel has been turned off',
-                    'success'
-                )
-                .then(function () {
-                    dataTable.ajax.reload();
-                });
-            //     }
-            // });
-        }
-    })
-});
