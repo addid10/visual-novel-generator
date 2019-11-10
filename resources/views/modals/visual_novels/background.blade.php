f05c343618f5c2cb6897be5782b5a5eda4b486f3
addid10 <rezkynewaditya@gmail.com>
2019-11-10 18:34:29 +0800

    add id on background table, delete dummy

--- resources/views/modals/visual_novels/background.blade.php
+++ resources/views/modals/visual_novels/background.blade.php
@@ -1,15 +1,15 @@
-<div class="modal fade" id="visual-novel-characters-modal" tabindex="-1" role="dialog">
+<div class="modal fade" id="visual-novel-backgrounds-modal" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Progress Pengerjaan</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Visual Novel's Backgrounds</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <div class="table-responsive">
-                        <table class="table table-bordered">
+                        <table class="table table-bordered" id="visual-novel-backgrounds-table">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -18,31 +18,9 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>21-01-2019</td>
-                                    <td>Konsultasi Pra-Proposal</td>
-                                    <td>
-                                        <button class="btn btn-icon btn-gradient-danger btn-rounded"><span
-                                                class="mdi mdi-close-circle"></span></button>
-                                    </td>
-                                </tr>
                             </tbody>
                         </table>
-    
                     </div>
-                    <hr>
-                    <form>
-                        <div class="row">
-                            <div class="form-group col-md-6">
-                                <label for="exampleFormControlInput1">Name</label>
-                                <select class="form-control form-control-sm" name="" id="">
-                                    <option value="">Main</option>
-                                </select>
-                            </div>
-                        </div>
-                        <button type="button" class="btn btn-gradient-success btn-rounded w-100">Add</button>
-                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
