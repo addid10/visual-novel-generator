aea24e0ef4f606010b1e212127028c0a1a78a232
addid10 <rezkynewaditya@gmail.com>
2019-11-11 02:16:45 +0800

    add dialogue function in game js

--- public/js/game/game.js
+++ public/js/game/game.js
@@ -1,10 +1,14 @@
+// CSRF 
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
 let audios = [];
 let sceneNumber = parseInt($('#dialogue-number').val());
-let characterName;
-let characterImage;
-let dialogue;
-let bacgkround;
-
+let playing = true;
+let audio, characterName, characterImage, background;
 
 // Stop Other Music
 function isPlaying(audelem) {
@@ -18,10 +22,47 @@ function isPlaying(audelem) {
     }
 }
 
-function dialogue(id, number) {
+function nextDialogue(number) {
     $.ajax({
-        url: "GET",
+        type: "GET",
+        url: "next",
+        data: {
+            number: number
+        },
+        success: function (data) {
+            if (data.id !== undefined) {
+                console.log(data)
+                audio = audios[data.music.name];
 
+                isPlaying(audio);
+
+                audios[data.music.name].play();
+
+                if (playing == false) {
+                    playing = true;
+                } else {
+                    playing = false;
+                }
+
+                if (background !== data.background.image) {
+                    $('.background').css('background-image', 'url(../../storages/' + data.background.image + ')');
+                    background = data.background.image;
+                }
+
+                if (characterName !== data.character_image.character.nickname) {
+                    $('#character-name').html('<span class="animated jackInTheBox delay-2s">' + data.character_image.character.nickname + '</span>');
+                    characterName = data.character_image.character.nickname;
+                }
+
+                if (characterImage !== data.character_image.image) {
+                    $('#character-faceclaim').html('<img class="animated fadeIn delay-2s mx-auto character-faceclaim" src="../../storages/' + data.character_image.image + '"> ');
+                    characterImage = data.character_image.image;
+                }
+
+                $('#dialogue').text(data.dialogue);
+                $('#dialogue-number').text(data.dialogue_number);
+            }
+        }
     })
 }
 
@@ -40,4 +81,7 @@ $(document).ready(function () {
     })
 })
 
-$(document).on('click, keydown', '.')
+$(document).click(function () {
+    nextDialogue(sceneNumber);
+    sceneNumber++
+});
