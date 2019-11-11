d80dc5d71577bf2fc8cab2f4e5735c64ddb40fc9
addid10 <rezkynewaditya@gmail.com>
2019-11-11 17:25:24 +0800

    add guarded table in SaveData model

--- app/SaveData.php
+++ app/SaveData.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SaveData extends Model
 {
     protected $table = 'save_datas';
+    protected $guarded = ['created_at', 'updated_at'];
     
     public function story()
     {
