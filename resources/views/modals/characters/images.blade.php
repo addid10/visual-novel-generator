fb1ac0368cf5013a6c358af8304730c913cb3a2a
addid10 <rezkynewaditya@gmail.com>
2019-11-13 01:49:09 +0800

    delete dummy images

--- resources/views/modals/characters/images.blade.php
+++ resources/views/modals/characters/images.blade.php
@@ -2,19 +2,13 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="character-images-title">Modal title</h5>
+                <h5 class="modal-title" id="character-images-title">Character Images</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="row mt-3">
-                  <div class="col-6 pr-1">
-                    <img src="{{ asset('assets/images/dashboard/img_1.jpg') }}" class="mb-2 mw-100 w-100 rounded" alt="image">
-                  </div>
-                  <div class="col-6 pl-1">
-                    <img src="{{ asset('assets/images/dashboard/img_3.jpg') }}" class="mw-100 w-100 rounded" alt="image">
-                  </div>
+                <div class="row mt-3" id="character-images">
                 </div>
             </div>
             <div class="modal-footer">
