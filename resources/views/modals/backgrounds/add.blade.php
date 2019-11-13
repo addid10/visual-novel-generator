93e37bf037a2c1009c015aa6478e2597f9e179e8
addid10 <rezkynewaditya@gmail.com>
2019-11-14 07:47:28 +0800

    add new input for multiple select visual novels

--- resources/views/modals/backgrounds/add.blade.php
+++ resources/views/modals/backgrounds/add.blade.php
@@ -1,28 +1,35 @@
 <div class="modal fade" id="background-modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="background-title">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form id="background-form">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="name">Background Name</label>
-                            <input type="text" id="name" class="form-control form-control-sm" name="name" required>
-                        </div>
-                        <div class="form-group">
-                                <label for="images">Images</label>
-                                <input type="file" class="form-control-file" id="images" name="image">
-                            </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" id="background-action">Save</button>
-                    </div>
-                </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="background-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form id="background-form">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="name">Background Name</label>
+                        <input type="text" class="form-control form-control-sm" id="name" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="images">Images</label>
+                        <input type="file" class="form-control-file" id="image" name="image" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="topics">Visual Novels</label>
+                        <select multiple class="form-control form-control-sm" id="visual-novels" name="visual_novels[]">
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" id="background-id">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" id="background-action">Save</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
