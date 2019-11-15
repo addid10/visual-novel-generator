cc36fb889998117727898f28e01c8a35e4703127
addid10 <rezkynewaditya@gmail.com>
2019-11-15 12:57:00 +0800

    crud js

--- public/js/music/music.js
+++ public/js/music/music.js
@@ -71,6 +71,119 @@ $('#music-table tbody').on('click', '.play-music', function () {
 
 });
 
+
+$(document).ready(function () {
+    $.ajax({
+        url: '../visual-novels',
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            if (result.data !== undefined) {
+                result.data.forEach(function (result) {
+                    let visualNovels = '<option value="' + result.id + '">' + result.title + '</option>';
+                    $('#visual-novels').append(visualNovels);
+                })
+            }
+        }
+    })
+
+})
+
+
+// Click Button Add
+$('#music-add').click(function () {
+    $('#music-form')[0].reset();
+    $('#music-title').text("Add music");
+    $('#music-action').text("Add");
+    $('#music').attr('required', '');
+});
+
+//Fetch datas for update
+$('#music-table tbody').on('click', '.update', function () {
+    let id = $(this).attr('id');
+    let visualNovels = [];
+    $.ajax({
+        url: "musics/" + id + "/edit",
+        dataType: "json",
+        success: function (result) {
+            $('#music-modal').modal('show');
+            $('#music-title').text("Update music");
+            $('#music-action').text("Update");
+
+            result.visual_novels.forEach(function (visualNovel) {
+                visualNovels.push(visualNovel.id);
+            });
+
+
+            $('#name').val(result.name);
+            $('#visual-novels').val(visualNovels);
+            $('#music-id').val(result.id);
+            $('#music').removeAttr('required');
+        }
+    })
+});
+
+// Store
+$(document).on('submit', '#music-form', function (e) {
+    e.preventDefault();
+
+    //Variables
+    let action = $('#music-action').text();
+    let id = $('#music-id').val();
+    let url;
+
+    let formData = new FormData(this);
+
+    if (action == "Add") {
+        url = "musics";
+    } else if (action == "Update") {
+        url = "musics/" + id;
+        formData.append("_method", "PUT");
+    }
+
+    if (url !== '') {
+        Swal.fire({
+            title: 'Loading',
+            timer: 60000,
+            onBeforeOpen: () => {
+                Swal.showLoading()
+            }
+        });
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                $('#music-form')[0].reset();
+
+                if (data.error == undefined) {
+                    Swal.fire({
+                            type: 'success',
+                            title: data.success,
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        .then(function () {
+                            dataTable.ajax.reload();
+                            $('#music-modal').modal('hide');
+                        });
+                } else {
+                    Swal.fire({
+                        type: 'error',
+                        title: data.error,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            }
+        });
+    }
+});
+
+// Delete music
 $('#music-table tbody').on('click', '.delete', function () {
     let id = $(this).attr("id");
 
@@ -84,20 +197,28 @@ $('#music-table tbody').on('click', '.delete', function () {
         confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
         if (result.value) {
-            // $.ajax({
-            //     url: "lecturers/" + id,
-            //     type: 'DELETE',
-            //     success: function () {
-            Swal.fire(
-                    'Deleted Id ' + id + '!',
-                    'Visual novel has been turned off',
-                    'success'
-                )
-                .then(function () {
-                    dataTable.ajax.reload();
-                });
-            //     }
-            // });
+            Swal.fire({
+                title: 'Loading',
+                timer: 60000,
+                onBeforeOpen: () => {
+                    Swal.showLoading()
+                }
+            });
+
+            $.ajax({
+                url: "musics/" + id,
+                type: 'DELETE',
+                success: function () {
+                    Swal.fire(
+                            'Deleted.',
+                            'Music successfully deleted!',
+                            'success'
+                        )
+                        .then(function () {
+                            dataTable.ajax.reload();
+                        });
+                }
+            });
         }
     })
 });
