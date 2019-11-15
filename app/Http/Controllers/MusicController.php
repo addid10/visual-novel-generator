f47cc9fd59831dd9fa2222d13732513c6b4b2180
addid10 <rezkynewaditya@gmail.com>
2019-11-15 12:57:08 +0800

    method crud

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -26,4 +26,101 @@ class MusicController extends Controller
             'data' => $musics
         ]);
     }
+
+    public function store(Request $request)
+    {
+        try {
+            
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|unique:musics,name',
+            ]);
+
+            if($validator->fails()) {
+                return response()->json([
+                    'error' => "The music has already been taken!"
+                ]);
+            }
+            
+            $music = new Music;
+
+            $music->name = $request->name;
+            
+            
+            if ($request->hasFile('music')) {
+
+                $audioName = UploadHelper::uploadFile(
+                    $request->file('music'), 
+                    $request->name,
+                    'musics'
+                );
+
+                $music->music = $audioName;
+            }
+            $music->save();
+            $music->visual_novels()->attach($request->visual_novels);
+
+            return response()->json([
+                'success' => "Data added successfully!"
+            ]);
+       
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => "Failed data execution!"
+            ]);
+        }
+
+    }
+    
+    public function edit($id)
+    {
+        $music = Music::with('visual_novels')->findOrFail($id);
+
+        return response()->json($music);
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        try {
+            $music = Music::findOrFail($id);
+
+            $music->name = $request->name;
+
+            if ($request->hasFile('music')) {
+                UploadHelper::deleteFile($music->music);
+
+                $audioName = UploadHelper::uploadFile(
+                    $request->file('music'), 
+                    $request->name,
+                    'musics'
+                );
+
+                $music->music = $audioName;
+            }
+
+            $music->save();
+            $music->visual_novels()->sync($request->visual_novels);
+            
+            return response()->json([
+                'success' => "Data updated successfully!"
+            ]);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => "Failed data execution!",
+                'message' => $th
+            ]);
+        }
+    }
+
+
+    public function destroy($id)
+    {
+        $music = Music::findOrFail($id);
+        
+        UploadHelper::deleteFile($music->music);
+
+        $music->delete();
+        $music->visual_novels()->detach();
+    }
 }
