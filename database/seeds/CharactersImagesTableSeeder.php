19abfe94c0de3a3e085c85a526c4ef550ef186a9
addid10 <rezkynewaditya@gmail.com>
2019-11-16 19:42:54 +0800

    change on seeder

--- database/seeds/CharactersImagesTableSeeder.php
+++ database/seeds/CharactersImagesTableSeeder.php
@@ -14,19 +14,19 @@ class CharactersImagesTableSeeder extends Seeder
         if (DB::table('characters_images')->get()->count() == 0) {
             DB::table('characters_images')->insert([
                 [
-                    'image' => 'characters/Fendy.png',
+                    'image' => 'characters/Fendy_1573719379_5dcd0d539a94d',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                     'character_id' => '2',
                 ],
                 [
-                    'image' => 'characters/Kagami.png',
+                    'image' => 'characters/Kagami_1573719364_5dcd0d44d2f26',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                     'character_id' => '1',
                 ],
                 [
-                    'image' => 'characters/Kagami-smile.png',
+                    'image' => 'characters/Kagami_1573719365_5dcd0d4514cf0',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                     'character_id' => '1',
