b80186192a9e4888b98b8f681c2403201ded5884
addid10 <rezkynewaditya@gmail.com>
2019-12-26 19:59:21 +0800

    fix background seeder

--- database/seeds/BackgroundsTableSeeder.php
+++ database/seeds/BackgroundsTableSeeder.php
@@ -15,13 +15,13 @@ class BackgroundsTableSeeder extends Seeder
             DB::table('backgrounds')->insert([
                 [
                     'name' => 'BG-School-1',
-                    'image' => '01',
+                    'image' => 'backgrounds/BG-School-1_1573719315_5dcd0d13b9951',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ],
                 [
                     'name' => 'BG-School-2',
-                    'image' => '02',
+                    'image' => 'backgrounds/BG-School-2_1573719329_5dcd0d2139597',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ]
